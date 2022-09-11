24c45f115e60ecbf31e7aa022a980d635f2cb5e2
Annushka1997 <annushkach1997@mail.ru>
2022-09-12 01:40:53 +0400

    Test

---
+++ Lesson 39/script.js
@@ -0,0 +1,96 @@
+"use strict";
+
+
+/*
+mkdir 
+touch
+rm
+rm -rf
+git init
+ls
+cd ..
+cd
+cd ~
+date
+start
+code .
+U-untracked
+Tracked
+Commit 
+rm -rf .git {
+    remove folder
+}
+rm -fr .git {
+    remove folder
+}
+git status 
+git add . 
+modify
+git add -A {
+
+}
+git rm --cached {
+    Делаем антрекед 
+}
+git rm -r --cached . {
+    Делает все антрекед
+}
+git rm -r --cached index.html {
+    Делает все антрекед
+}
+modified {
+    Когда мы что-то изменяем нужно обязательно делать
+    адд,потому что мы если так сделаем пул,он загрузиться с первой 
+}
+git restore index.html {
+    Удаляем наши изменения
+}
+git restore .
+git commit -M 'Name'
+.gitignore {
+    Игнорирует те файлы,которые нам не нужны
+    .error
+    node_modules/
+    .pip
+}
+git log {
+    Информация
+    Кеш
+    Head
+    Author
+    Date
+    Name of commit 
+}
+git show {
+    Информация,но более глубже 
+    Дата,
+    Автор,
+}
+q если хотим выйти
+git config --global user.name "";
+git config --global user.email "";
+// branch
+git branch "layout" - Создание
+git branch - Проверка
+git checkout layout - переходим на другую ветку 
+merge{
+    Соединение нескольких веток
+    Нужно перейти обязательно с той ветки,потому что
+    данная ветка удаляется
+    git merge front 
+}
+// Удаление ветки {
+git branch -D "front"
+    Нужно обязательно сделать refresh
+}
+git clone 
+git pull {
+    Отслеживание изменений 
+} 
+
+*/
+
+/*
+Есть который работает на локал,а есть и который работает на сервер
+Ветка для того,чтобы мы могли работать сразу над одним проектом
+*/
